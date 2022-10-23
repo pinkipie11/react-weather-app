c746ad01684d623dffc5e94bab71a6a1b6d801af
pinkipie11 <stani.simic22@gmail.com>
2022-10-23 09:01:35 +0200

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'e1c9587882mshccaa3a2642db3f6p1d5768jsnb60142c0aede', // enter your rapid api key here
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'd94f8fd934d4a9a976ae9ba5f6185e13'; // enter your key from openweather API
